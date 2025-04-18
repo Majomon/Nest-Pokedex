7341dbe5ec6937e5e81040f72c64d654fde681f0
Mauri <mauri.monzon91@gmail.com>
2025-04-18 19:38:01 -0300

    Global prefix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 // Los modulos siempre van en los imports
 @Module({
@@ -8,6 +9,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PokemonModule,
   ],
   controllers: [],
   providers: [],
